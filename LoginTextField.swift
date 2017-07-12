c6b80f4cba5226e86da76e58b1b1fbf31e137443
SKE <appybuildmore@gmail.com>
2017-07-11 19:33:08 -0500

    1. Followed the Brian Advent video from: https://www.youtube.com/watch?v=DYhPufgcqF4&t=172s to complete the login screen.

---
+++ LoginTextField.swift
@@ -0,0 +1,30 @@
+//
+//  LoginTextField.swift
+//  NreecsNigol
+//
+//  Created by Sain-R Edwards Jr. on 7/11/17.
+//  Copyright © 2017 Sain-R Edwards Jr. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class LoginTextField: UITextField {
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.layer.borderColor = UIColor(white: 231 / 255, alpha: 1).cgColor
+        self.layer.borderWidth = 1
+        
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 8, dy: 7)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return textRect(forBounds: bounds)
+    }
+
+}
